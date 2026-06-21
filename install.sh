76e9b7c4dfe63462bae3a3b126d766889370c995
Lakshya <lakshyachauhan147@gmail.com>
2026-06-21 18:39:58 +0000

    update in installer.sh

--- install.sh
+++ install.sh
@@ -2,22 +2,27 @@
 
 set -e
 
-echo "Installing Procx..."
+echo ""
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo " PROCX Installer"
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo ""
+
 
 # url for reaching the realese
 URL="https://github.com/lakshya-8000cr/procx/releases/download/v1.0.0/procx"
 
-sudo curl -L $URL -o /usr/local/bin/procx #download the file from url  then sve it to location /usr/local/bin/... okkk
+echo "Downloading Procx..."
+sudo curl -fsSL "$URL" -o /usr/local/bin/procx  #download the file from url  then sve it to location /usr/local/bin/... okkk
 
-
-# permissions 
-sudo chmod +x /usr/local/bin/procx  # make the binary executable 
+echo "Setting permissions..."
+sudo chmod +x /usr/local/bin/procx
 
 echo ""
-echo "Procx installed successfully!" #this will show on terminal
+echo "Procx installed successfully."
 echo ""
 echo "Try:"
-echo ""
-echo "procx list"
-echo "procx diagnose <pid>"
+echo "  procx list"
+echo "  procx diagnose <pid>"
+echo "  procx port <port>"
 echo ""
